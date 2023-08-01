a0b32e53d18bf40d81dcb6944b649f60d7acfbda
Askar <askar@9o1d.com>
2023-08-01 21:38:36 +0700

    Fix prime.mod.h

--- prime/prime.mod.h
+++ prime/prime.mod.h
@@ -1,3 +1,15 @@
+/*
+ * File:  prime.mod.h
+ * Description: Prime generator module sample
+ * Author: Askar Azhibaev
+ * Date:  May 2021
+ * Copyright (c) 2021-2023  Askar Azhibaev
+ * 
+ * This file is part of C3 extension.
+ * URL: https://azhibaev.com/c3.zip
+ * License: CPOL http://www.codeproject.com/info/cpol10.aspx
+ */
+
 #ifndef PRIME_H
 #define PRIME_H
 
